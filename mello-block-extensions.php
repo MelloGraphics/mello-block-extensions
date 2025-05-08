5ef3a6204fb7d7ea80aa817c823188797dc99e37
Ashley Pickering <ash@bjmdigital.com.au>
2025-05-08 22:39:54 +1000

    update auto update function and re export

--- mello-block-extensions.php
+++ mello-block-extensions.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Mello Block Extensions
  * Plugin URI:        https://mellographics.com/
  * Description:       Custom block extensions and functionality for WordPress block themes.
- * Version:           2.0.17
+ * Version:           2.1.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Ashley Pickering
@@ -15,6 +15,8 @@
  */
 
 namespace Mello;
+use YahnisElsts\PluginUpdateChecker\v5p5\Vcs\GitHubApi;
+use YahnisElsts\PluginUpdateChecker\v5p5\Vcs\PluginUpdateChecker;
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
@@ -27,15 +29,19 @@ require_once plugin_dir_path(__FILE__) . 'inc/admin-settings.php';
 // Include the Plugin Update Checker library
 require __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
 
-// Set up auto-updates from GitHub
-$updateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/MelloGraphics/mello-block-extensions',
+// Set up the GitHub API client for updates
+$githubApi = new GitHubApi(
+    'https://github.com/MelloGraphics/mello-block-extensions/',
+    '' // If private, put your personal access token here
+);
+$githubApi->enableReleaseAssets();
+
+// Create the update checker instance
+$updateChecker = new PluginUpdateChecker(
+    $githubApi,
     __FILE__,
     'mello-block-extensions'
 );
-
-// Use the ZIP assets from GitHub releases
-$updateChecker->getVcsApi()->enableReleaseAssets();
 
 register_activation_hook(__FILE__, function () {
 	if (function_exists('Mello\\mello_initialize_enabled_extensions')) {
