b8331c580705f665e88e266e2a101aee85612c2a
Ashley Pickering <ash@bjmdigital.com.au>
2025-05-08 22:13:54 +1000

    Finish work for release

--- mello-block-extensions.php
+++ mello-block-extensions.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Mello Block Extensions
  * Plugin URI:        https://mellographics.com/
  * Description:       Custom block extensions and functionality for WordPress block themes.
- * Version:           2.0.1
+ * Version:           2.0.17
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Ashley Pickering
@@ -23,6 +23,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once plugin_dir_path(__FILE__) . 'inc/helpers.php';
 require_once plugin_dir_path(__FILE__) . 'inc/enqueue-assets.php';
 require_once plugin_dir_path(__FILE__) . 'inc/admin-settings.php';
+
+// Include the Plugin Update Checker library
+require __DIR__ . '/plugin-update-checker/plugin-update-checker.php';
+
+// Set up auto-updates from GitHub
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/MelloGraphics/mello-block-extensions',
+    __FILE__,
+    'mello-block-extensions'
+);
+
+// Use the ZIP assets from GitHub releases
+$updateChecker->getVcsApi()->enableReleaseAssets();
 
 register_activation_hook(__FILE__, function () {
 	if (function_exists('Mello\\mello_initialize_enabled_extensions')) {
