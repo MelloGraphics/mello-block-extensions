f15707713c8f8adcca92bdafdf35ef1276bf06ef
Ashley Pickering <ash@bjmdigital.com.au>
2025-05-13 16:08:49 +1000

    Swiper editor style update for 'fit silde'

--- build/blocks/swiper/index.asset.php
+++ build/blocks/swiper/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => '8353604877bacab804d9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-dom-ready', 'wp-i18n'), 'version' => 'ce82b2ab4b3057fe680f');
