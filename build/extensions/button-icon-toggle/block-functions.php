dd0a0a2099782cc05033a8505a717fe5cb2bfd7f
Ashley Pickering <ash@bjmdigital.com.au>
2025-06-15 13:30:23 +1000

    add first test for icon button toggle

---
+++ build/extensions/button-icon-toggle/block-functions.php
@@ -0,0 +1,50 @@
+<?php
+// Add this to your plugin's main PHP file or functions.php
+
+// Hook into the render_block filter to modify button output
+add_filter('render_block', 'mello_add_icon_to_button_block', 10, 2);
+
+function mello_add_icon_to_button_block($block_content, $block) {
+    // Only process core/button blocks
+    if ($block['blockName'] !== 'core/button') {
+        return $block_content;
+    }
+    
+    // Check if icon is enabled and we have the necessary attributes
+    $attrs = $block['attrs'] ?? [];
+    $icon_enabled = $attrs['iconEnabled'] ?? false;
+    $icon_type = $attrs['iconType'] ?? 'none';
+    $icon_glyph = $attrs['iconGlyph'] ?? '';
+    $icon_image_url = $attrs['iconImageURL'] ?? '';
+    
+    // Exit early if icon is not enabled or type is none
+    if (!$icon_enabled || $icon_type === 'none') {
+        return $block_content;
+    }
+    
+    // Generate icon HTML
+    $icon_html = '';
+    
+    if ($icon_type === 'fa' && !empty($icon_glyph)) {
+        // Font Awesome icon - don't escape HTML entities for glyphs
+        $icon_html = sprintf(
+            '<span class="mello-button-icon mello-fa-icon" aria-hidden="true" style="margin-right: 0.5em;">%s</span>',
+            $icon_glyph
+        );
+    } elseif ($icon_type === 'image' && !empty($icon_image_url)) {
+        // Image icon
+        $icon_html = sprintf(
+            '<img class="mello-button-icon mello-image-icon" src="%s" alt="" style="width: 1em; height: 1em; margin-right: 0.5em; vertical-align: middle;">',
+            esc_url($icon_image_url)
+        );
+    }
+    
+    // If we have icon HTML, inject it into the button link
+    if (!empty($icon_html)) {
+        $pattern = '/(<a[^>]*class="[^"]*wp-block-button__link[^"]*"[^>]*>)/i';
+        $replacement = '$1' . $icon_html;
+        return preg_replace($pattern, $replacement, $block_content);
+    }
+    
+    return $block_content;
+}
---
+++ src/extensions/button-icon-toggle/edit.js
@@ -0,0 +1,138 @@
+import {
+    InspectorAdvancedControls,
+    MediaUpload,
+    MediaUploadCheck,
+} from "@wordpress/block-editor";
+import {
+    Button,
+    SelectControl,
+    TextControl,
+    ToggleControl,
+} from "@wordpress/components";
+import { createHigherOrderComponent } from "@wordpress/compose";
+import { Fragment } from "@wordpress/element";
+import { addFilter } from "@wordpress/hooks";
+import { __ } from "@wordpress/i18n";
+
+const ICON_TYPES = [
+    { label: __("None", "mello-block-extensions"), value: "none" },
+    { label: __("Font Awesome Icon", "mello-block-extensions"), value: "fa" },
+    { label: __("Image", "mello-block-extensions"), value: "image" },
+];
+
+const addIconControls = (BlockEdit) => {
+    return (props) => {
+        const { attributes, setAttributes, isSelected, name } = props;
+
+        if (name !== "core/button") return <BlockEdit {...props} />;
+
+        const {
+            iconEnabled = false,
+            iconType = "none",
+            iconGlyph = "",
+            iconImageID,
+            iconImageURL,
+        } = attributes;
+
+        return (
+            <Fragment>
+                <BlockEdit {...props} />
+                {isSelected && (
+                    <InspectorAdvancedControls>
+                        <ToggleControl
+                            __next40pxDefaultSize
+                            label={__("Enable Icon", "mello-block-extensions")}
+                            checked={iconEnabled}
+                            onChange={(value) => setAttributes({ iconEnabled: value })}
+                        />
+                        {iconEnabled && (
+                            <>
+                                <SelectControl
+                                    __next40pxDefaultSize
+                                    label={__("Icon Type", "mello-block-extensions")}
+                                    value={iconType}
+                                    options={ICON_TYPES}
+                                    onChange={(value) => setAttributes({ iconType: value })}
+                                />
+
+                                {iconType === "fa" && (
+                                    <TextControl
+                                        className="mello-font-awesome-input"
+                                        __next40pxDefaultSize
+                                        label={__("Font Awesome Glyph", "mello-block-extensions")}
+                                        value={iconGlyph}
+                                        onChange={(value) => setAttributes({ iconGlyph: value })}
+                                        help={__("Enter the glyph (e.g. \\f238) or paste the icon glyph", "mello-block-extensions")}
+                                    />
+                                )}
+
+                                {iconType === "image" && (
+                                    <MediaUploadCheck>
+                                        <MediaUpload
+                                            onSelect={(media) =>
+                                                setAttributes({
+                                                    iconImageID: media.id,
+                                                    iconImageURL: media.url,
+                                                })
+                                            }
+                                            allowedTypes={["image"]}
+                                            value={iconImageID}
+                                            render={({ open }) => (
+                                                <Button onClick={open} isSecondary>
+                                                    {iconImageURL
+                                                        ? __("Replace image", "mello-block-extensions")
+                                                        : __("Select image", "mello-block-extensions")}
+                                                </Button>
+                                            )}
+                                        />
+                                    </MediaUploadCheck>
+                                )}
+                            </>
+                        )}
+                    </InspectorAdvancedControls>
+                )}
+            </Fragment>
+        );
+    };
+};
+
+addFilter(
+    "editor.BlockEdit",
+    "mello-block-extensions/button-icon-controls",
+    createHigherOrderComponent(addIconControls, "withIconControls")
+);
+
+const addIconAttributes = (settings, name) => {
+    if (name !== "core/button") return settings;
+
+    settings.attributes = {
+        ...settings.attributes,
+        iconEnabled: {
+            type: "boolean",
+            default: false,
+        },
+        iconType: {
+            type: "string",
+            default: "none",
+        },
+        iconGlyph: {
+            type: "string",
+            default: "",
+        },
+        iconImageID: {
+            type: "number",
+            default: null,
+        },
+        iconImageURL: {
+            type: "string",
+            default: "",
+        },
+    };
+    return settings;
+};
+
+addFilter(
+    "blocks.registerBlockType",
+    "mello-block-extensions/button-icon-attributes",
+    addIconAttributes
+);
